f635819026e5fc5622c550954feafe9bc2d40560
MrAnAcondA <shahzebjadoon0@gmail.com>
2022-01-18 14:33:49 +0500

    finalizing app

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserSearch from '../users/UserSearch';
+import UsersResult from '../users/UsersResult';
+
+const Home = () => {
+  return (
+    <div>
+      <UserSearch />
+      <UsersResult />
+    </div>
+  );
+};
+
+export default Home;
